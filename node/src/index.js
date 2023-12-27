def7c5fcf4b6dbb51f6beee2bd534974b351b4f6
hernancaceres <harinatha777@hotmail.com>
2023-12-27 20:12:52 -0300

    creando tablas

--- node/src/index.js
+++ node/src/index.js
@@ -2,12 +2,12 @@ import app from "./app.js";
 import { sequelize } from "./database/database.js";
 import {PORT} from "./config.js"
 
-// import "../src/models/Project.js"
+import "../src/models/Project.js"
 // import "../src/models/Task.js"
 
 
 async function main() {
-  await sequelize.sync({force: false});
+  await sequelize.sync({force: true});
   app.listen(PORT);
   console.log("Server on port 4000");
 }
