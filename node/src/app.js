e2ad21e911afc1d18e341fc66a8cca261fa4aa61
hernancaceres <harinatha777@hotmail.com>
2023-12-28 13:12:50 -0300

    probando integracion en produccion4

--- node/src/app.js
+++ node/src/app.js
@@ -17,10 +17,12 @@ app.use(express.json());
 //     credentials: true,
 // }));
 
-app.use(cors({
-    origin: "https://node-react-render-postgresql-front.onrender.com",  // Reemplaza con la URL de tu aplicación de React
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: "https://node-react-render-postgresql-front.onrender.com",  // Reemplaza con la URL de tu aplicación de React
+//     credentials: true,
+// }));
+
+app.use(cors());
 
 // Routes
 app.use("/api/projects", projectRoutes);
